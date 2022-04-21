783b462e4a6693131ff53e886d5d35b2ff81a7bb
João Martins <up201707311@fe.up.pt>
2022-04-21 16:19:29 +0100

    Add channel shutdown signal after sending logs

--- logs_sender.py
+++ logs_sender.py
@@ -1,7 +1,8 @@
-# pylint: disable=C0111
+# pylint: disable=C0111,consider-using-f-string
 """Dummy sender that sends log formatted data, acting as logstash output"""
 import os
 import multiprocessing as mp
+import time
 import pika
 
 __location__ = os.path.realpath(
@@ -13,13 +14,17 @@ def dummy_sender(filename: str, exchange: str):
         pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.exchange_declare(exchange=exchange, exchange_type='direct', durable=True)
+    channel.exchange_declare(exchange=exchange, durable=True)
     with open(os.path.join(__location__, filename), 'r', encoding='utf-8') as log_file:
 
         for log in log_file:
             channel.basic_publish(exchange=exchange, routing_key=exchange, body=log)
-        connection.close()
     print('Sent logs in {}!'.format(filename))
+    time.sleep(6)
+    print('Sending channel shutdown signal.')
+    channel.exchange_declare(exchange='channel-stop', durable=True)
+    channel.basic_publish(exchange='channel-stop',routing_key='',body='')
+    connection.close()
 
 if __name__ == '__main__':
     first_sender = mp.Process(
